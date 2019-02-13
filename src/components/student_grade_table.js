4649e1f4dde6b6fd9fff54f8bef50d258acbbe4b
Matthew Staniszewski <34016859+staniszmatt@users.noreply.github.com>
2019-02-13 15:28:41 -0800

    Revert "Add and created students to the server and display the changes from the server. "

--- src/components/student_grade_table.js
+++ src/components/student_grade_table.js
@@ -2,24 +2,9 @@ import React from 'react';
 import StudentRow from './student_row';
 
 const StudentGradeTable = (props) => {
-	const { studentList } = props;
-	let studentRows = []; //We will assign something in here, so no need to assign in this case, may be better too for error checking
-
-	if (Array.isArray(studentList) && studentList.length){
-		studentRows = props.studentList.map((student)=>{
-			return <StudentRow delete={props.deleteStudent} key={student.id} student={student}/>
-		});
-	} else {
-		studentRows.push(
-			<tr key="no-data" >
-				<td colSpan="4" >
-					<h4 className="center grey-text" >No Student Data Available</h4>
-				</td>
-			</tr>
-		);
-	}
-	
-
+	const studentRows = props.studentList.map((student)=>{
+		return <StudentRow delete={props.deleteStudent} key={student.id} student={student}/>
+	});
 	return (
 		<table>
 			<thead>
